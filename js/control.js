c87ac2f954bcff60e34d0aaff19b6716097d5ccf
Cesar Valenciano Fonseca <www.cavf.20@gmail.com>
2020-05-22 12:36:56 -0500

    V220520
    - Reporte CI disponible para Nominas y Laborales

--- js/control.js
+++ js/control.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     let nivel_usuario = document.querySelector('#nivel_usuario').value;
     let empleado_activo = document.querySelector('#empleado_activo').value;
 
-    let version = 'V.220520DEV';
+    let version = 'V.220520';
 
     $('#version').html(version);
 
