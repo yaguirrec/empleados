1fa7ab005f8ad34e9cac9ec572c644ebe5cd21b0
César Valenciano Fonseca <www.cavf.20@gmail.com>
2022-10-07 17:20:18 -0500

    PROD Excel export
    Se actualizo el SRC del plugin table2Excel del footer al header, asi como la version de 1.1.0 a 1.1.1

--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -5,7 +5,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
 
 <!-- SWEEALERT 2 -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8.8.1/dist/sweetalert2.all.min.js"></script>
