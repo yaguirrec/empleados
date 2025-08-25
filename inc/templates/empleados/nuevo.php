445f11d0c594b52300a158ecaa0b6c2d57604162
FernyMexQ <ldelacruz@mexq.com.mx>
2025-08-25 09:20:00 -0600

    fix(modificar.php, nuevo.php, reingreso.php): add options G to J

--- inc/templates/empleados/nuevo.php
+++ inc/templates/empleados/nuevo.php
@@ -86,6 +86,10 @@
           <option value="D">D</option>
           <option value="E">E</option>
           <option value="F">F</option>
+          <option value="G">G</option>
+          <option value="H">H</option>
+          <option value="I">I</option>
+          <option value="J">J</option>
 
         </select>
       </div>
